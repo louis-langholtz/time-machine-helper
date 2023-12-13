636f6ac525ad17ceac6eef94f236052da6369bcd
Louis Langholtz <louis-langholtz@users.noreply.github.com>
2023-12-13 14:21:31 -0700

    Adds table state management & clear functionality to Settings

--- settings.cpp
+++ settings.cpp
@@ -8,10 +8,14 @@ namespace {
 constexpr auto tmutilPathKey = "tmutilPath";
 constexpr auto tmutilStatTimeKey = "tmutilStatusInterval";
 constexpr auto tmutilDestTimeKey = "tmutilDestinationsInterval";
-constexpr auto mainWindowGeomKey = "mainWindowGeomtry";
-constexpr auto mainWindowStateKey = "mainWindowState";
 constexpr auto sudoPathKey = "sudoPath";
 constexpr auto pathInfoTimeKey = "pathInfoInterval";
+constexpr auto mainWindowGeomKey = "mainWindowGeomtry";
+constexpr auto mainWindowStateKey = "mainWindowState";
+constexpr auto destinationsTableStateKey = "destinationsTableState";
+constexpr auto machinesTableStateKey = "machinesTableState";
+constexpr auto volumesTableStateKey = "volumesTableState";
+constexpr auto backupsTableStateKey = "backupsTableState";
 
 constexpr auto defaultTmutilPath = "/usr/bin/tmutil";
 constexpr auto defaultTmutilStatTime = 1000;
@@ -45,16 +49,6 @@ auto tmutilDestInterval() -> int
                             defaultTmutilDestTime).toInt();
 }
 
-auto mainWindowGeometry() -> QByteArray
-{
-    return settings().value(mainWindowGeomKey).toByteArray();
-}
-
-auto mainWindowState() -> QByteArray
-{
-    return settings().value(mainWindowStateKey).toByteArray();
-}
-
 auto sudoPath() -> QString
 {
     return settings().value(sudoPathKey,
@@ -65,6 +59,36 @@ auto pathInfoInterval() -> int
 {
     return settings().value(pathInfoTimeKey,
                             defaultPathInfoTime).toInt();
+}
+
+auto mainWindowGeometry() -> QByteArray
+{
+    return settings().value(mainWindowGeomKey).toByteArray();
+}
+
+auto mainWindowState() -> QByteArray
+{
+    return settings().value(mainWindowStateKey).toByteArray();
+}
+
+auto destinationsTableState() -> QByteArray
+{
+    return settings().value(destinationsTableStateKey).toByteArray();
+}
+
+auto machinesTableState() -> QByteArray
+{
+    return settings().value(machinesTableStateKey).toByteArray();
+}
+
+auto volumesTableState() -> QByteArray
+{
+    return settings().value(volumesTableStateKey).toByteArray();
+}
+
+auto backupsTableState() -> QByteArray
+{
+    return settings().value(backupsTableStateKey).toByteArray();
 }
 
 void setTmutilPath(const QString& value)
@@ -82,6 +106,16 @@ void setTmutilDestInterval(int value)
     settings().setValue(tmutilDestTimeKey, value);
 }
 
+void setSudoPath(const QString &value)
+{
+    settings().setValue(sudoPathKey, value);
+}
+
+void setPathInfoInterval(int value)
+{
+    settings().setValue(pathInfoTimeKey, value);
+}
+
 void setMainWindowGeometry(const QByteArray &value)
 {
     settings().setValue(mainWindowGeomKey, value);
@@ -92,14 +126,59 @@ void setMainWindowState(const QByteArray &value)
     settings().setValue(mainWindowStateKey, value);
 }
 
-void setSudoPath(const QString &value)
+void setDestinationsTableState(const QByteArray &value)
 {
-    settings().setValue(sudoPathKey, value);
+    settings().setValue(destinationsTableStateKey, value);
 }
 
-void setPathInfoInterval(int value)
+void setMachinesTableState(const QByteArray &value)
 {
-    settings().setValue(pathInfoTimeKey, value);
+    settings().setValue(machinesTableStateKey, value);
+}
+
+void setVolumesTableState(const QByteArray &value)
+{
+    settings().setValue(volumesTableStateKey, value);
+}
+
+void setBackupsTableState(const QByteArray &value)
+{
+    settings().setValue(backupsTableStateKey, value);
+}
+
+void clear()
+{
+    settings().clear();
+}
+
+void removeMainWindowGeometry()
+{
+    settings().remove(mainWindowGeomKey);
+}
+
+void removeMainWindowState()
+{
+    settings().remove(mainWindowStateKey);
+}
+
+void removeDestinationsTableState()
+{
+    settings().remove(destinationsTableStateKey);
+}
+
+void removeMachinesTableState()
+{
+    settings().remove(machinesTableStateKey);
+}
+
+void removeVolumesTableState()
+{
+    settings().remove(volumesTableStateKey);
+}
+
+void removeBackupsTableState()
+{
+    settings().remove(backupsTableStateKey);
 }
 
 }
--- settings.h
+++ settings.h
@@ -9,18 +9,35 @@ namespace Settings {
 auto tmutilPath() -> QString;
 auto tmutilStatInterval() -> int;
 auto tmutilDestInterval() -> int;
-auto mainWindowGeometry() -> QByteArray;
-auto mainWindowState() -> QByteArray;
 auto sudoPath() -> QString;
 auto pathInfoInterval() -> int;
+auto mainWindowGeometry() -> QByteArray;
+auto mainWindowState() -> QByteArray;
+auto destinationsTableState() -> QByteArray;
+auto machinesTableState() -> QByteArray;
+auto volumesTableState() -> QByteArray;
+auto backupsTableState() -> QByteArray;
 
 void setTmutilPath(const QString& value);
 void setTmutilStatInterval(int value);
 void setTmutilDestInterval(int value);
-void setMainWindowGeometry(const QByteArray& value);
-void setMainWindowState(const QByteArray& value);
 void setSudoPath(const QString& value);
 void setPathInfoInterval(int value);
+void setMainWindowGeometry(const QByteArray& value);
+void setMainWindowState(const QByteArray& value);
+void setDestinationsTableState(const QByteArray& value);
+void setMachinesTableState(const QByteArray& value);
+void setVolumesTableState(const QByteArray& value);
+void setBackupsTableState(const QByteArray& value);
+
+void clear();
+
+void removeMainWindowGeometry();
+void removeMainWindowState();
+void removeDestinationsTableState();
+void removeMachinesTableState();
+void removeVolumesTableState();
+void removeBackupsTableState();
 
 }
 
